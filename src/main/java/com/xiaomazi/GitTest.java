0c439790e6d28b56bfba887dbf3d42bd7db33a6e
xiaomazi <xiaomazi@bjpowernode.com>
2022-10-07 16:57:21 +0800

    master second commit

--- src/main/java/com/xiaomazi/GitTest.java
+++ src/main/java/com/xiaomazi/GitTest.java
@@ -3,5 +3,6 @@ package com.xiaomazi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git1");
+        System.out.println("Hello git2");
     }
 }
